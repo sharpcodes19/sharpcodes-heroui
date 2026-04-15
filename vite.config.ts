6866e2f8e3fc719df420610357ff8bb1fa4c885b
Jae Jarabe <jaesharp19@gmail.com>
2026-04-15 21:57:00 +0800

    update types

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-import dts from "vite-plugin-dts"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
 	plugins: [
-		react(),
-		dts({
-			insertTypesEntry: true
-		})
+		react()
 	],
 	build: {
 		lib: {
